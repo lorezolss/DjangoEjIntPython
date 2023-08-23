e5333de684a165d91c8a3c77133240839e39f857
Lorena <zolsslorena@gmail.com>
2023-08-23 14:27:03 -0300

    Subo dos ejercicios mas (el 5 y 6)

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,1 +1,23 @@
 #Lee un valor entero y lo devuelve mientras sea correcto, sino itera hasta que lo sea
+def get_int_iterativo():
+    ok = False
+    while not ok:
+        numero = input('Ingrese un numero entero positivo: ')
+        try:
+            nro = int(numero)
+        except ValueError:
+            print ('No ingresó un número válido')
+        else:
+            ok = True
+    return nro
+
+def get_int_recursivo():
+    numero = input('Ingrese un número entero positivo: ')
+    try:
+        nro = int(numero)
+    except ValueError:
+        print ('No ingresó un número válido')
+        return get_int_recursivo()
+    else:
+        return nro
+    
--- ejercicio_6.py
+++ ejercicio_6.py
@@ -1,32 +1,51 @@
 # Crea clase Persona
 class Persona:
     def __init__(self,nombre,edad,dni) -> None:
-        self.nombre = nombre
-        self.edad = edad
-        self.dni = dni
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__dni = dni
 
     @property
     def nombre(self):
-        return self.nombre
+        return self.__nombre
     
     @property
     def edad(self):
-        return self.edad
+        return self.__edad
     
     @property
     def dni(self):
-        return self.dni
+        return self.__dni
     
     @nombre.setter
     def nombre(self,nombre):
-        self.nombre = nombre
+        self.__nombre = nombre
 
     @edad.setter
     def edad(self,edad):
-        self.edad = edad
+        if edad < 0:
+            print (f'La edad debe ser mayor a 0')
+        else:
+            self.__edad = edad
+
+    def valido_dni(self,dni):
+        try:
+            documento = int(dni)
+        except ValueError:
+            print (f'No es un DNI válido, deben ser números')
+        if len(str(documento)) < 7 or len(str(documento)) > 8:
+            print(f'No es un número válido para un DNI')
 
     @dni.setter
     def dni(self,dni):
-        self.dni = dni
+        self.valido_dni(dni)
+        self.__dni = dni
+
+    def mostrar(self):
+        return f'Nombre: {self.nombre}, DNI: {self.dni} y tiene {self.edad} años.'
+    
+    def es_mayor_de_edad (self):
+        return self.edad >= 18
+    
 
     
